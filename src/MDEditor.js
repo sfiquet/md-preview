0d5736ba94008d07c03d2244baf888660c720d8c
Sylvie Fiquet <sfiquetdev@gmail.com>
2019-01-13 15:01:05 +0000

    Rewrite default markdown text

--- src/MDEditor.js
+++ src/MDEditor.js
@@ -11,11 +11,27 @@ renderer.link = (href, title, text) => {
 };
 
 const defaultText = 
-`# Header
-## Subheader
-Link example: [FreeCodeCamp](https://freecodecamp.org)
+`# Using Markdown
+In the words of its creator John Gruber:
+> Markdown’s syntax is intended for one purpose: to be used as a format for *writing* for the web.
 
-Here's some inline code: \`const myNumber = 42;\` and here's a code block:
+Markdown allows you to concentrate on your content without being distracted by the HTML syntax.
+<br />
+
+## Basics
+---
+### Headers
+Start a line with one to six \`#\` to define a header.
+
+### Styling text
+Add emphasis to text by wrapping it in asterisks \`*\` or underscores \`_\`: Use one pair for *italics*, two for **bold**, three for ***both***. You can also **mix _and_ match**.
+
+Any of Markdown's special characters will be treated as normal characters if you escape them with \`\\\`, e.g. you can write \\_italics\\_ instead of _italics_.
+
+### Code
+You can style inline code by wrapping it between backticks \`\`\` like this: \`const myNumber = 42;\`
+
+Wrap block code between a pair of triple backticks:
 \`\`\`
 let i = 0;
 while (i < 10){
@@ -24,19 +40,41 @@ while (i < 10){
 }
 \`\`\`
 
-Here's a list:
+### Lists
+You can write an unordered list with \`*\`, \`-\` or \`+\`:
 - egg
-- flours
-- tomatoes
++ flour
+* tomatoes
 
-quote:
-> Here's a quote
+You can write an ordered list with a number followed by a period:
+1. one
+2. two
+3. three
 
-An image:
+### Quote
+You can quote a text by preceding it with \`>\`:
+> In a hole in the ground there lived a hobbit. Not a nasty, dirty, wet hole, filled with the ends of worms and an oozy smell, nor yet a dry, bare, sandy hole with nothing in it to sit down on or to eat: it was a hobbit-hole, and that means comfort.
+― J.R.R. Tolkien, The Hobbit
 
-![React Logo w/ Text](https://goo.gl/Umyytc)
+### Links
+To include a link, such as [freeCodeCamp](https://www.freecodecamp.org), use the following format:
+\`[text](url)\`
 
-Text can be in *italics* or **bold** or ***both***
+### Image
+To include an image, use the following format:
+\`![alt text](url)\`
+
+![Spiral galaxy NGC 4414](https://upload.wikimedia.org/wikipedia/commons/c/c3/NGC_4414_%28NASA-med%29.jpg)
+
+<br />
+
+## Syntax Reference
+---
+You can find a lot more details about Markdown here:
+
+- [Standard Markdown](https://daringfireball.net/projects/markdown/syntax): by John Gruber, the creator of Markdown.
+
+- [Github Flavored Markdown](https://help.github.com/articles/basic-writing-and-formatting-syntax/) or GFM: an extended version of Markdown specifically for use on Github.
 `;
 
 class MDEditor extends Component {
