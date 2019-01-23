7513f73e3aa8404014610d9bdf28484086a56e63
Sylvie Fiquet <sfiquetdev@gmail.com>
2019-01-23 19:35:07 +0000

    Improve header title

--- src/App.js
+++ src/App.js
@@ -6,8 +6,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title"><span className="App-title-markdown">Markdown</span> <span className="App-title-previewer">Previewer</span></h1>
+        <header className="App-header p-1">
+          <div className="App-title">
+            <span className="App-title-markdown d-inline-block">Markdown</span>
+            <span className="App-title-previewer d-inline-block align-bottom">Previewer</span>
+          </div>
         </header>
         <main className="container pt-2 pb-4">
           <MDEditor></MDEditor>
